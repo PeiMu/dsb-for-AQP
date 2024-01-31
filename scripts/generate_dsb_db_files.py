e9984e993c2caa81d13056f85124e9fb5122d1c4
PeiMu <ds1231h@gmail.com>
2024-01-31 15:47:10 +0000

    update generate data file

--- scripts/generate_dsb_db_files.py
+++ scripts/generate_dsb_db_files.py
@@ -2,20 +2,20 @@ import os
 import subprocess
 import shutil
 
-bin_dir = r'D:\code\v2.11.0rc2\tools'
-bin_path = os.path.join(bin_dir, 'dsdgen.exe')
+bin_dir = os.getcwd()
+bin_path = os.path.join(bin_dir, 'dsdgen')
 
-scale = 10
-file_dir = # directory of database files
+scale = 5
+file_dir = bin_dir + "/out"  # directory of database files
 
 if os.path.exists(file_dir):
-	shutil.rmtree(file_dir)
+    shutil.rmtree(file_dir)
 
 if not os.path.exists(file_dir):
-	os.makedirs(file_dir)
+    os.makedirs(file_dir)
 
 # WARNING: This code hasn't been valiated. Somewhat the dir parameter is not
-# taken in to account by the code. The output db files are generated in bin_dir
-cmd = [bin_path, '/scale', str(scale), '/terminate', 'N', '/force', '/dir',  file_dir]
+# taken into account by the code. The output db files are generated in bin_dir
+cmd = [bin_path, '-scale', str(scale), '-terminate', 'N', '-force', 'Y', '-dir', file_dir]
 print(cmd)
-subprocess.call(cmd, cwd = bin_dir)
+subprocess.call(cmd, cwd=bin_dir)
