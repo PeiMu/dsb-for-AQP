facd0452daccab307a86af4700199f4eb89523f8
Pei <ds1231h@gmail.com>
2025-04-15 18:59:48 +0000

    update script

--- scripts/prepare_QuerySplit_queries.sh
+++ scripts/prepare_QuerySplit_queries.sh
@@ -6,6 +6,9 @@ cp -r ../code/tools/1_instance_out/ ../code/tools/1_instance_out_QuerySplit/
 cp edit_query.sh ../code/tools/1_instance_out_Official/
 cp edit_QS_query.sh ../code/tools/1_instance_out_QuerySplit/
 
-pushd ../code/tools/1_instance_out_QuerySplit/
+pushd ../code/tools/1_instance_out_Official/
 bash ./edit_query.sh
+popd
+pushd ../code/tools/1_instance_out_QuerySplit/
+bash ./edit_QS_query.sh
 popd
