d82d2f75787299b9e3a1506a758048a1bbafc1ab
pei <ds1231h@gmail.com>
2025-05-08 18:31:23 +0100

    upload queries template for QuerySplit

--- scripts/prepare_QuerySplit_queries.sh
+++ scripts/prepare_QuerySplit_queries.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cp -r ../code/tools/1_instance_out/ ../code/tools/1_instance_out_Official/
-cp -r ../code/tools/1_instance_out/ ../code/tools/1_instance_out_QuerySplit/
+cp -r ../code/tools/1_instance_out_qs/ ../code/tools/1_instance_out_qs_Official/
+cp -r ../code/tools/1_instance_out_qs/ ../code/tools/1_instance_out_qs_QuerySplit/
 
-cp edit_query.sh ../code/tools/1_instance_out_Official/
-cp edit_QS_query.sh ../code/tools/1_instance_out_QuerySplit/
+cp edit_query.sh ../code/tools/1_instance_out_qs_Official/
+cp edit_QS_query.sh ../code/tools/1_instance_out_qs_QuerySplit/
 
-pushd ../code/tools/1_instance_out_Official/
+pushd ../code/tools/1_instance_out_qs_Official/
 bash ./edit_query.sh
 popd
-pushd ../code/tools/1_instance_out_QuerySplit/
+pushd ../code/tools/1_instance_out_qs_QuerySplit/
 bash ./edit_QS_query.sh
 popd
