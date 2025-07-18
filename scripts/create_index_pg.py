df55af792f4b59237892383e812290d3ec174b67
PeiMu <ds1231h@gmail.com>
2025-07-18 17:01:42 +0100

    anomymize scripts

--- scripts/create_index_pg.py
+++ scripts/create_index_pg.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 1:
     scale = sys.argv[1]
 
 db_name = r'dsb_' + str(scale) # database name
-bin_path = r'/home/pei/Project/project_bins/bin/postgres' # binary of Postgres
+bin_path = YOUR_POSTGRES_PATH # e.g, r'xxx/bin/postgres' # binary of Postgres
 sql_path = r'../../scripts/dsb_index_pg.sql'
 
 # start database service
